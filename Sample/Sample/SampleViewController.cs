130930b5af9216bd5b10f80a6935c15c4cc28dc2
Josh Hogan <jhogan@mailmasterminds.com>
2013-10-06 18:33:25 -0400

    Add basic sample project

---
+++ Sample/Sample/SampleViewController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using TSMessages;
+
+namespace Sample
+{
+	public partial class SampleViewController : UIViewController
+	{
+		public SampleViewController () : base ("SampleViewController", null)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			showButton.TouchUpInside += (object sender, EventArgs e) => {
+
+				// Show the TSMessage
+				TSMessage.ShowNotification ("This is the sample message.", TSMessageNotificationType.Success);
+
+			};
+			                            
+		}
+	}
+}
+
